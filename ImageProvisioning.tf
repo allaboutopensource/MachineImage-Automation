8563fc655f7b08c4c15f135df2342b7fcf89ce8b
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2020-07-23 14:16:30 +0530

    Create ImageProvisioning.tf

---
+++ ImageProvisioning.tf
@@ -0,0 +1,24 @@
+provider "openstack" {
+  user_name   = "username"
+  tenant_name = "project-name"
+  password    = "password"
+  auth_url    = "https://openstack-URL:5000/v3"
+  tenant_id   =  "project-ID"
+  region      = "us-east-1"
+  user_domain_name    =   "company.com"
+  project_domain_id   =   "project-domain-ID"
+}
+
+resource "openstack_compute_instance_v2" "terraform-vm" {
+
+  name            = "automated-vm"
+  count =         2
+  image_id        = "b4889017-2028-40a3-aedd-c5a97c7de6f1"
+  flavor_id       = "3"
+  key_pair        = "openstack-key"
+  security_groups = ["default"]
+
+  network {
+    name = "Project-network"
+  }
+}
