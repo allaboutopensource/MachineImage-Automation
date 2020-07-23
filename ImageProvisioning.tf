0e1ff9f5cf91ef042e5cd43611ca82ee97025bed
Sunil kathait <5890851+allaboutopensource@users.noreply.github.com>
2020-07-23 14:25:28 +0530

    Update ImageProvisioning.tf

--- ImageProvisioning.tf
+++ ImageProvisioning.tf
@@ -9,11 +9,16 @@ provider "openstack" {
   project_domain_id   =   "project-domain-ID"
 }
 
-resource "openstack_compute_instance_v2" "terraform-vm" {
+data "openstack_images_image_v2" "centos_packer" {
+   most_recent = true
+   tag  = "packer-image"
+}
 
-  name            = "automated-vm"
-  count =         2
-  image_id        = "Image-ID"
+resource "openstack_compute_instance_v2" "terraform_vm" {
+
+  for_each        =  var.instance_name
+  name            =  each.key
+  image_id        = data.openstack_images_image_v2.centos_packer.id
   flavor_id       = "3"
   key_pair        = "openstack-key"
   security_groups = ["default"]
@@ -21,4 +26,16 @@ resource "openstack_compute_instance_v2" "terraform-vm" {
   network {
     name = "Project-network"
   }
+}
+
+resource "openstack_networking_floatingip_v2" "floating_ip" {
+   pool = "floating-ip-pool"
+   for_each        =  var.instance_name
+}
+
+
+resource "openstack_compute_floatingip_associate_v2" "fip_associate" {
+   for_each        =  var.instance_name
+   floating_ip = openstack_networking_floatingip_v2.floating_ip[each.key].address
+   instance_id = openstack_compute_instance_v2.terraform_vm[each.key].id
 }
